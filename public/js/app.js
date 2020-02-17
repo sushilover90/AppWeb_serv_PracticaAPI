7020c296a418af203ae1487110d5123a831ce872
Edgar Herrera <edgarinelfuerte@hotmail.com>
2020-02-17 09:31:42 -0600

    Merge Isra.

--- public/js/app.js
+++ public/js/app.js
@@ -51322,8 +51322,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\guill\Desktop\APIPesada\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\guill\Desktop\APIPesada\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! Y:\escuela\,utt\,4to_cuatrimestre\appweb_servicios\nuevo_api\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! Y:\escuela\,utt\,4to_cuatrimestre\appweb_servicios\nuevo_api\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
